7c866b9cea153106ba0b5e2e26beaa05cd62a8b8
Yana Redkina <yana.redkina@icloud.com>
2021-08-19 17:57:15 +0300

    fixing Last method

--- database.cpp
+++ database.cpp
@@ -8,49 +8,13 @@
 using namespace std;
 
 
-// void Database::Add(const Date& date, const string& event) {
-//     if (EventsMapWithVector.count(date) == 0) {
-//         EventsMapWithVector[date].push_back(event);
-//     } else if (find(EventsMapWithVector.at(date).begin(), EventsMapWithVector.at(date).end(), event) == EventsMapWithVector.at(date).end()) {
-//         EventsMapWithVector[date].push_back(event);
-//     }
-// }
 
 void Database::Add(const Date& date, const string& event) {
-    if (EventsMapWithVector.count(date) == 0) {
-        EventsMapWithVector[date].push_back(event);
-    } else if (find(EventsMapWithVector.at(date).begin(), EventsMapWithVector.at(date).end(), event) == EventsMapWithVector.at(date).end()) {
+    if (EventsMapWithSet.count(date) == 0 || EventsMapWithSet.at(date).count(event) == 0) {
+        EventsMapWithSet[date].insert(event);
         EventsMapWithVector[date].push_back(event);
     }
-    EventsMapWithSet[date].insert(event);
 }
-
-// int Database::RemoveIf(function<bool(const Date& date, const string& event)> predicate) {
-//     int count = 0;
-//     for (auto& item: EventsMapWithVector) {
-//         for (auto it = item.second.begin(); it != item.second.end();) {
-//             if (predicate(item.first, *it)) {
-//                 it = item.second.erase(it);
-//                 ++count;
-//             } else {
-//                 ++it;
-//             }
-//         }
-//     }
-//     DelEmptyValuesFromMap();
-//     return count;
-// }
-
-
-// int Database::RemoveIf(function<bool(const Date& date, const string& event)> predicate) {
-//     int count = 0;
-//     for (auto& item: EventsMapWithVector) {
-//         auto it = stable_partition(item.second.begin(), item.second.end(), predicate());
-//         item.second.erase(it, item.second.end());
-//     }
-//     DelEmptyValuesFromMap();
-//     return count;
-// }
 
 int Database::RemoveIf(function<bool(const Date& date, const string& event)> predicate) {
     int count = 0;
@@ -68,20 +32,17 @@ int Database::RemoveIf(function<bool(const Date& date, const string& event)> pre
     return count;
 }
 
-// vector<string> Database::FindIf(function<bool(const Date& date, const string& event)>predicate) const {
-//     vector<string> result;
-//     stringstream stream;
-//     for (auto item: EventsMapWithVector) {
-//         for (auto element : item.second) {
-//             if (predicate(item.first, element)) {
-//                 stream << item.first << " " << element;
-//                 result.push_back(stream.str());
-//                 stream.str("");
-//             }
-//         }
+
+// int Database::RemoveIf(function<bool(const Date& date, const string& event)> predicate) {
+//     int count = 0;
+//     for (auto& item: EventsMapWithVector) {
+//         auto it = stable_partition(item.second.begin(), item.second.end(), predicate());
+//         item.second.erase(it, item.second.end());
 //     }
-//     return result;
+//     DelEmptyValuesFromMap();
+//     return count;
 // }
+
 
 vector<string> Database::FindIf(function<bool(const Date& date, const string& event)>predicate) const {
     vector<string> result;
@@ -98,20 +59,10 @@ vector<string> Database::FindIf(function<bool(const Date& date, const string& ev
     return result;
 }
 
-// string Database::Last(const Date& date) const {
-//     auto it = EventsMapWithVector.upper_bound(date);
-//     if (it == EventsMapWithVector.begin() && it != EventsMapWithVector.end()) {
-//         throw invalid_argument("No entries");
-//     } else {
-//         stringstream stream;
-//         stream << prev(it)->first << " " << prev(it)->second.back();
-//         return stream.str();
-//     }
-// }
 
 string Database::Last(const Date& date) const {
     auto itS = EventsMapWithSet.upper_bound(date);
-    if (itS == EventsMapWithSet.begin() && itS != EventsMapWithSet.end()) {
+    if (itS == EventsMapWithSet.begin()) {
         throw invalid_argument("No entries");
     } else {
         Date lastDate = prev(itS)->first;
@@ -125,14 +76,6 @@ string Database::Last(const Date& date) const {
     }
 }
 
-// ostream& Database::Print(ostream& out) const {
-//   for (const auto& item : EventsMapWithVector) {
-//     for (const auto& event : item.second) {
-//       out << item.first << " " << event << endl;
-//     }
-//   }
-//   return out;
-// }
 
 ostream& Database::Print(ostream& out) const {
   for (const auto& item : EventsMapWithVector) {
